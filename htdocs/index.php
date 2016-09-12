7017198fd11caffb3b055428fb6f3c6a7ff7a1e1
Jack Lepird <jack.lepird@gmail.com>
2016-09-12 13:07:00 +0000

    switched to php

--- htdocs/index.php
+++ htdocs/index.php
@@ -108,6 +108,9 @@
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam mauris, vulputate ut nisi pharetra, gravida aliquet erat. Pellentesque iaculis lobortis tortor, eu eleifend eros fringilla sed. Donec consequat purus eu sem pellentesque, vel porttitor mi aliquam. Vivamus ornare dolor eleifend consequat vestibulum. Etiam eleifend neque eu mauris aliquam consectetur. Ut feugiat nulla quis nisi tempor ornare vitae ac enim. Nam consectetur id nulla in congue. Sed et odio quis ante fermentum finibus ut sed massa. Suspendisse maximus gravida lorem vitae sagittis. Integer consectetur augue magna, molestie placerat ligula rhoncus ac. Proin dictum, lacus sit amet semper euismod, orci lacus condimentum dui, nec blandit lorem metus semper dui. 
+            <?php 
+            echo "foo";
+            ?>
         </p>
 
             <p class="text-right">
