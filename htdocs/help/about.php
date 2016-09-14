18f3af10b7d15172d0fc61b5860587a248c01f74
Courtney Knoth <courtney.l.knoth.civ@mail.mil>
2016-09-14 19:16:08 +0000

    added project objective to about page

--- htdocs/help/about.php
+++ htdocs/help/about.php
@@ -9,8 +9,17 @@
 <div class="col-md-3">  </div>
     <div class="col-md-5">
     <br> <br> <br> 
-        <p> Placeholder about info</p>
-
+        <p>The objective of this study is to quantify differences between the status quo assignment
+           process and the proposed algorithmic system.  This contrast will be measured using</p>
+           statistics such as the following:</p>
+            <ul>Number of officers given their first, second, third etc. assignment
+                preferences under each system.</ul>
+            <ul>Number of commanders given their first, second, third, etc. assignment
+                preferences under each system.</ul>
+            <ul>Sentiment surveys of affected officers, commanders, and units.</ul>
+            <ul>Compliance rate with applicable assignment policy, to include policies derived
+                from public law, joint regulations, Air Force Instructions, and the applicable
+                Career Field Manager (CFM).</ul>
     </div>
 
 </div>
