2822bcaf9bb34df50dc42eb4b58cdf18d77ef4db
Jay <jaypsanchez@gmail.com>
2016-09-30 14:50:01 +0000

    make myvector open in new window

--- htdocs/officers/find_officer.php
+++ htdocs/officers/find_officer.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION["uname"])) {
     <br> <br> <br>
     <p>You will be able to search for experienced officers via the Air Force MyVECTOR database. While searching through the website, it is recommended you take notes and create a prioritized list of preferred officers that you find suitable for each one of your billets. Please submit this prioritized list through the "My Preference List" link above by 28 February 2017.
 
-<p><a href="https://afvec.langley.af.mil/myvector/">Proceed to MyVECTOR</a>
+<p><a href="https://afvec.langley.af.mil/myvector/" target="_blank">Proceed to MyVECTOR</a>
     </div>
 
 
