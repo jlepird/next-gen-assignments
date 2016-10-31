8f5524c8ac7423026ae76d25076feab971b69298
Jay <jaypsanchez@gmail.com>
2016-10-31 14:01:50 +0000

    modified about page

--- htdocs/help/about.php
+++ htdocs/help/about.php
@@ -9,10 +9,22 @@
 <div class="col-md-3">  </div>
     <div class="col-md-5">
     <br> <br> <br> 
+    <h3>Background</h3>
         <p>The objective of this test is to quanitfy the feasibilty and performance of a proposed talent-to-assignment matching algorithm, in contrast to the status quo assignment process.  This effort is in response to the SecDef's Force of the Future initiative to implement a web-based talent management system. Please keep in mind that is only a test and will have no bearing on the actual VML cycle, as the FY17 Summer VML has already closed. Keeping that in mind, we ask that you participate as if this new assignment system is part has already been implemented.  The more accurate and quantifiable data we can collect, the better we can make recommendations to Air Force leadership. </p>
-        <p>The backbone of this test is a talent matching algorithm that maximizes satisfaction between both officers and the billet owners themselves.  To allow the algorithm to function at its maximum potential, it requires cooperation from both parties.  Officers need to create a 1-to-n list of assignments that they want, and billet owners need to create a 1-to-n list of officers that they want.</p>
+    <h3>Testing a new talent matching algorithm</h3>
+        <p>The backbone of this assignment system is to test a new talent matching algorithm that maximizes satisfaction between both officers and the billet owners themselves.  To allow the algorithm to function at its maximum potential, it requires cooperation from both parties.  Officers need to create a 1-to-n list of assignments that they want, and billet owners need to create a 1-to-n list of officers that they want.  The algorithm results would require further modifications by the AFPC Assignment Team to incorporate additional constraints such as unit manning requirements.</p>
+    <h3>Helping officers "shop" for their next assignment</h3>
         <p>To aid in the creation of preferential assignment lists, this website also incorporates an easier way to "shop" for assignments compared to the current AMS system.  The <a href="../billets/find.php">Find a Billet</a> link allows officers to filter and view details about the assignments that are available.  The information listed for each assignment is more than what is typically shown for assignments during past VML cycles.  This information is collected from the billet owners themselves using using the <a href="../billets/manage.php">Manage Billets</a> link.  As such, if you are a billet owner, please provide as much detail about the assignment.</p>
+    <h3>Helping billet owners find the right officers</h3>
         <p>To aid billet owners in shopping for potential officers, the existing MyVector website has the list of available officers along with thei. If you an officer on the VML, it will be beneficial for you to log into MyVector and fill out your profile with as much information regarding your talents, skills, expertise, or anything that will make you look favorable as billet owners search through the website to build their preferred officer list.</p>
+        
+    <h3>Deadlines</h3>
+        <p>Officers to provide information on themselves via <a href="https://afvec.langley.af.mil/myvector/" target="_blank">MyVector</a>. Billet owners to provide information on their assignments via <a href="../billets/manage.php">Manage Billets</a>. <b>Deadline: 3 February 2017</b></p>
+        
+        <p>Officers to input their preferred assignment list. Billet owners to input their preferred officers list.  This is done via the <a href="/prefs/input.php">My Preference List</a> link.  <b>Deadline: 3 March 2017</b></p>
+        
+        
+        
         <!--This contrast will be measured using</p>
            statistics such as the following:</p>
             <ul>Number of officers given their first, second, third etc. assignment
