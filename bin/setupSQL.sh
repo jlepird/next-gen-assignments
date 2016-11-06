f52e655eafa6959ca53b70a1c4c634894fa44f8c
Jack Lepird <jack.lepird@gmail.com>
2016-11-06 15:40:07 -0500

    fixed bash aliasing issue

--- bin/setupSQL.sh
+++ bin/setupSQL.sh
@@ -3,15 +3,15 @@
 # Download the psql binary 
 curl https://s3.amazonaws.com/18f-cf-cli/psql-9.4.4-ubuntu-14.04.tar.gz | tar xvz
 
-alias psql="./psql/bin/psql"
+psql="./psql/bin/psql"
 
 # Run the commands to initialize the database
-psql $DATABASE_URL -f createTables.sql
+$psql $DATABASE_URL -f createTables.sql
 
-psql $DATABASE_URL -c "\copy coreCodes from '`pwd`/../data/coreCodes.csv' with CSV;"
+$psql $DATABASE_URL -c "\copy coreCodes from '`pwd`/../data/coreCodes.csv' with CSV;"
 
-psql $DATABASE_URL -c "\copy allowableDegrees from '`pwd`/../data/aadCodes.csv' with CSV;"
+$psql $DATABASE_URL -c "\copy allowableDegrees from '`pwd`/../data/aadCodes.csv' with CSV;"
 
-psql $DATABASE_URL -c "\copy acqLevels from '`pwd`/../data/acqCodes.csv' with CSV;"
+$psql $DATABASE_URL -c "\copy acqLevels from '`pwd`/../data/acqCodes.csv' with CSV;"
 
-psql $DATABASE_URL -c "delete from coreCodes where txt like '%RESERVE%' or right(txt, 4) = 'HIST';"
+$psql $DATABASE_URL -c "delete from coreCodes where txt like '%RESERVE%' or right(txt, 4) = 'HIST';"
