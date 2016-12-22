71cacc76fcddbd98c0be14b9162026cbda5a62e8
Jay <jaypsanchez@gmail.com>
2016-12-22 18:48:37 +0000

    updated the update profile tutorial

--- myvectorprofile/myvectorprofile.php
+++ myvectorprofile/myvectorprofile.php
@@ -24,6 +24,10 @@ if (!isset($_SESSION["uname"])) {
 
 You will need to create an account if you do not already have one.<br>
 
+
+
+
+
 <H3>Update your personal profile.</H3>
 
 You should see the following screen after logging in.  <br><br>
@@ -37,21 +41,41 @@ Update all the applicable fields in your personal profile. As an officer, your s
 
 <br><br>Click save when complete.
 
+
+
+
+
 <H3>Update your assignment history and relevant work experience.</H3>
 
 
 <img src="see my experience.JPG" alt="See My Experience"><br><br>
 
-<p>Your duty distory should be automatically filled in.  The quick view table will display the following for each duty entry: Start date of the duty, Duty Title, AFSC, Component, Unit, Organization, Type, Location, MAJCOM, Rank, Career Field Experience Code (created by the CFM), Experience Description, and association request action.
+<p>Your duty distory should be automatically filled in.  The quick view table will display the following for each duty entry: Start date of the duty, Duty 
 
-<p>All duty entries are pulled from MilPDS if the information is incorrect, you must contact the local Military Personnel Section (MPS) to ensure the information is updated correctly within those systems. Note: The Rank associated with a duty is calculated from the Date of Rank held when you started the duty.
+Title, AFSC, Component, Unit, Organization, Type, Location, MAJCOM, Rank, Career Field Experience Code (created by the CFM), Experience Description, and 
 
-<p>You can click “Change Component” if your component is incorrect (options include the following: AF Component, Reserve, Air National Guard, and Civilian). Note: MyVECTOR is Total Force, therefore if your component is incorrect, it is important to correct. The Active Duty history of many reservists will initially display as Reserves due to the initial data source.
+association request action.
+
+<p>All duty entries are pulled from MilPDS if the information is incorrect, you must contact the local Military Personnel Section (MPS) to ensure the 
+
+information is updated correctly within those systems. Note: The Rank associated with a duty is calculated from the Date of Rank held when you started the 
+
+duty.
+
+<p>You can click “Change Component” if your component is incorrect (options include the following: AF Component, Reserve, Air National Guard, and Civilian). 
+
+Note: MyVECTOR is Total Force, therefore if your component is incorrect, it is important to correct. The Active Duty history of many reservists will 
+
+initially display as Reserves due to the initial data source.
 
 
-<br><br><img src="duty.JPG" alt="Review your Duty History"> <br> <br>
+<br><br><img src="duty.JPG" alt="Review your Duty History"> <br><br>
 
-<p> The "Other-Self Reported Jobs" section allows you to add experience garnered throughout your career that cannot be identified any place else (i.e. jobs with other services, deployment experience, previous military or civilian experience). In addition, you will be able to associate a code with this duty using the dropdowns so it will appear on your experience summaries.  You can also edit existing entries in this section.
+<p> The "Other-Self Reported" section allows you to add experience garnered throughout your career that cannot be identified any place else (i.e. jobs with 
+
+other services, deployment experience, previous military or civilian experience). In addition, you will be able to associate a code with this duty using the 
+
+dropdowns so it will appear on your experience summaries.  You can also edit existing entries in this section.
 
 
 <br><br><img src="edit self report.JPG" alt="See My Experience">
@@ -62,19 +86,64 @@ Update all the applicable fields in your personal profile. As an officer, your s
 
 <p>Click "Save" when finished.
 
+
+
+
+
 <H3>Update your education and training.</H3>
 
-<p>This section allows you to view a summary of your education and training information. You will have the ability to view the following tables with associated information:
+<p>This section allows you to view a summary of your education and training information. You will have the ability to view the following tables with 
+
+associated information:
 <UL>
-<LI><b>Education:</b> Identifies the individuals’ degree information to include the Code, description, level (Ex. MA, BA, CCAF, etc.), school, and award date.
-<LI><b>Professional Military Education (PME):</b> Identifies the individuals’ PME information to include the code, description, level (Ex. IDE, PDE, Course 14, etc.), residence status, and award date.
-<LI><b>Acquisition Certification:</b> Identifies acquisition certifications held by the individual and includes the code, description, level of certification and the award date.
-<LI><b>Training:</b> Identifies all training related courses updated in MilPDS/DCPDS for the individual and includes the code, description, level, school, and award date. Note: some sections on this table may be left blank if not applicable.
+<LI><b>Education:</b> Identifies the individuals’ degree information to include the Code, description, level (Ex. MA, BA, CCAF, etc.), school, and award 
+
+date.
+<LI><b>Professional Military Education (PME):</b> Identifies the individuals’ PME information to include the code, description, level (Ex. IDE, PDE, Course 
+
+14, etc.), residence status, and award date.
+<LI><b>Acquisition Certification:</b> Identifies acquisition certifications held by the individual and includes the code, description, level of certification 
+
+and the award date.
+<LI><b>Training:</b> Identifies all training related courses updated in MilPDS/DCPDS for the individual and includes the code, description, level, school, 
+
+and award date. Note: some sections on this table may be left blank if not applicable.
 </UL>
 
 You are also able to add additional education experience that is not automatically pulled from DCPDS, as shown below.
 
 <br><br><img src="education.JPG" alt="See My Experience">
+
+
+
+
+
+<H3>Update your Development Plan.</H3>
+
+<p>This section allows you to update your job preferences, update your desire for training and education, and update your career goals. Just click 
+
+"Development Plan" in the MyVector Dashboard.  Be sure to click the "Save" button in each tab after making changes.
+
+<br><br><img src="development plan.JPG" alt="Development Plan">
+
+
+<p>The "Job Preferences" tab displays your job preferences to be considered by the Development Teams during the vectoring process.
+
+<br><br><img src="job pref.bmp" alt="Job Preferences">
+
+<p>The "Developmental Education" tab allows you to update your desire for training and/or Professional Military Education programs. You can enter your developmental education goals 
+
+as well as recommended text for your Senior Rater to use in their endorsement comments when you meet a board. You will also be able to update your desired 
+
+PME course. You can also view your current and historical Developmental Education boards by selecting “Click Here”. NOTE: If you are a Captain or Major you 
+
+will also be able to update you Advanced Studies Group intent on this tab.
+
+<br><br><img src="dev edu.bmp" alt="Development Education">
+
+<p>The "Intent & Comments" tab allows you to outline any relevant details concerning your career plans, goals, answering developmental team group intent questions, and any other information you would like leadership to know about.
+
+<br><br><img src="intent.bmp" alt="Intent and Comments">
 
 
     </div>
